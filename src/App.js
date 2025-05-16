e15d6ec1c661b971b9dcffd7b9ac88663a55847d
Jeevanantham N <njeevanantham2004@gmail.com>
2025-05-16 05:31:35 +0530

    successfully completed

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function TodoApp() {
     <div className="container ">
       <div className="card " id="card" >
       <div className="card-body">
-      <h1 className="text-dark text-center card-title">Todo app</h1>
+      <h1 className="text-dark text-center card-title">Todo app with changes </h1>
 
       <form onSubmit={addTodo} className="mb-6 text-center d-flex justify-content-center mt-5" style={{ width: "100%", height: "100%" }}>
 
